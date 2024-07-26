1b97020732dded639a55b1d1856842c6ee86611b
Shanti <fishfinna12@gmail.com>
2024-07-25 20:31:55 -0700

    started on some of the requests

--- src/pages/main/home.tsx
+++ src/pages/main/home.tsx
@@ -1,3 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export function Home() {
-  return <p>Home</p>;
+  const [episodes, setEpisodes] = useState<File[]>([]);
+
+  useEffect(() => {
+    async function fetchEpisodes() {
+      const response = await axios.get(
+        "https://archive.org/metadata/doctor-who_202210/files"
+      );
+      setEpisodes(response.data.result);
+    }
+
+    fetchEpisodes();
+  }, []);
+
+  return <p>{JSON.stringify(episodes[0])}</p>;
 }
---
+++ src/types/files.ts
@@ -0,0 +1,13 @@
+export interface File {
+  name: string;
+  source: string;
+  mtime: string;
+  size: string;
+  md5: string;
+  crc32: string;
+  sha1: string;
+  format: string;
+  length: string;
+  width: string;
+  height: string;
+}
