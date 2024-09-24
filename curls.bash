669925ba5c678a00a1db2c477558171ed90ae2d8
Shanti <fishfinna12@gmail.com>
2024-09-23 17:03:40 -0700

    removed old notes about the curls

--- curls.bash
+++
@@ -1,6 +0,0 @@
-# get the data
-curl --location 'https://archive.org/metadata/doctor-who_202210'
-
-# example of loading the episode
-curl --location 'https://archive.org/0/items/doctor-who_202210/S01E01%20-%20An%20Unearthly%20Child.mp4'
-
